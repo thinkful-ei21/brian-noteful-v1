9e0d2c3543f452ea3e415e0035268886d60e2da8
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-21 15:47:28 -0400

    last push

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+app.get('/api/notes/:id', (req, res)=>{
+  const id = req.params.id;
+  res.json(data.find(item => item.id === Number(id)));
+});
+
+
+app.get('/api/notes/:searchTerm',(req, res) => {
+  const searchTerm = req.query.searchTerm;
+  const list = data.filter(function(item){
+    return item.title.includes(searchTerm);
+  });
+  res.json(list);
+});
+
+// ADD STATIC SERVER HERE
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
