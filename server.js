11328cb49810c6a0cffb74b215f48048d4ba9691
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-29 06:33:08 -0400

    added work

--- server.js
+++ server.js
@@ -92,10 +92,17 @@ app.use(function (err, req, res, next) {
   });
 });
 
+
+
 // ADD STATIC SERVER HERE
 
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+// Listen for incoming connections
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = app; // Export for testing
